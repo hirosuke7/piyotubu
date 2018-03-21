0c1dbf000d22cd400dea74ea1e4dfcb9b62b2469
hirosuke7 <captif7_de_2_anjelot8@ezweb.ne.jp>
2018-03-21 15:04:18 +0900

    謎のmigrationエラー

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  :recoverable, :rememberable, :trackable, :validatable
   has_many :comments
-  
+
+  validates :family_name, presence: true, length: { maximum: 20 }
+  validates :given_name,  presence: true, length: { maximum: 20 }
+  validates :image_url,   presence: true, length: { maximum: 200 }
+
   def full_name
     family_name + given_name
   end
