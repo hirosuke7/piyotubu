464d4102b2cb6a1ad9ba2c6b190881471ba22a3b
hirosuke7 <captif7_de_2_anjelot8@ezweb.ne.jp>
2018-03-22 10:52:33 +0900

    ひとまず完成

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Rails.application.config.assets.unknown_asset_fallback = true
+
 module Piyotubu
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
